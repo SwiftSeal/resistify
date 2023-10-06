7dbd0dea995c511504b9a81680859b0eb3eceb63
SwiftSeal <65286772+SwiftSeal@users.noreply.github.com>
2023-10-06 16:39:48 +0100

    some experiments

--- nlrexpress.py
+++ nlrexpress.py
@@ -29,6 +29,31 @@ def parse_jackhmmer(file) -> dict:
                 hmm_dict[name].append(value_dict)
                 i += 3
 
+def generateInputFile( seqData:dict, hmm_it1:dict, hmm_it2:dict) -> dict:
+    data = {}
+
+    for name in seqData:
+        seq = seqData[name]
+        data[name] = []
+
+        for i, aa in enumerate(seq):
+            data[name].append([])
+            for k, (key, val) in enumerate( hmm_it1[name][i].items() ):
+                data[name][-1].append(val)
+
+            if name in hmm_it2:
+                for k, (key, val) in enumerate( hmm_it2[name][i].items() ):
+                    data[name][-1].append(val)
+            else:
+                for k, (key, val) in enumerate( hmm_it1[name][i].items() ):
+                    data[name][-1].append(val)
+
+    return data
+
 #subprocess.run(["jackhmmer", "--cpu", "6" "-E", "1e-5", "--domE", "1e-5", "--chkhmm", "test", "sample.fasta", "~/scratch/NLRexpress/hmmer_db/targetDB.fasta"])
 
 jackhmmer_iteration_1 = parse_jackhmmer("test-1.hmm")
+
+with open("sample.fasta") as f:
+    for record in SeqIO.parse(f, "fasta"):
+        seq = record.seq
