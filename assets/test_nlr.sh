f2e06878a2d61d6786bacee731a2894cd1053459
SwiftSeal <moraysmith98@gmail.com>
2024-12-16 14:38:53 +0000

    fix tests

--- assets/test_nlr.sh
+++ assets/test_nlr.sh
@@ -4,11 +4,13 @@
 #SBATCH -c 16
 #SBATCH --mem=16G
 
-curl -o refplantnlr.fa -O "https://doi.org/10.1371/journal.pbio.3001124.s013"
-curl -o dm.fa.gz -O "https://spuddb.uga.edu/data/DM_1-3_516_R44_potato.v6.1.hc_gene_models.pep.fa.gz"
+source activate resistify_dev
 
-python -m resistify.main nlr refplantnlr.fa -o test_nlr_refplantnlr
-python -m resistify.main nlr dm.fa.gz -o test_nlr_dm
+curl -o $TMPDIR/refplantnlr.fa -LO "https://doi.org/10.1371/journal.pbio.3001124.s013"
+curl -o $TMPDIR/dm.fa.gz -O "https://spuddb.uga.edu/data/DM_1-3_516_R44_potato.v6.1.hc_gene_models.pep.fa.gz"
 
-python -m resistify.main nlr --coconat refplantnlr.fa -o test_nlr_refplantnlr_coconat
-python -m resistify.main nlr --coconat dm.fa.gz -o test_nlr_dm_coconat
+python -m resistify.main nlr $TMPDIR/refplantnlr.fa -o test_nlr_refplantnlr
+python -m resistify.main nlr $TMPDIR/dm.fa.gz -o test_nlr_dm
+
+python -m resistify.main nlr --coconat $TMPDIR/refplantnlr.fa -o test_nlr_refplantnlr_coconat
+python -m resistify.main nlr --coconat $TMPDIR/dm.fa.gz -o test_nlr_dm_coconat
--- assets/test_prr.sh
+++ assets/test_prr.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #SBATCH -J resistify_prr_test
 #SBATCH -p gpu
+#SBATCH --gpus=1
 #SBATCH -c 16
 #SBATCH --mem=16G
 
-curl -o dm.fa.gz -O "https://spuddb.uga.edu/data/DM_1-3_516_R44_potato.v6.1.hc_gene_models.pep.fa.gz"
+source activate resistify_dev
 
-python -m resistify.main prr dm.fa.gz -o test_nlr_dm
+curl -o $TMPDIR/dm.fa.gz -O "https://spuddb.uga.edu/data/DM_1-3_516_R44_potato.v6.1.hc_gene_models.pep.fa.gz"
+
+python -m resistify.main prr $TMPDIR/dm.fa.gz -o test_nlr_dm
