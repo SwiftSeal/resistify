337fac4f2992f223065932bbe768ec16030a2011
SwiftSeal <moraysmith98@gmail.com>
2026-07-14 16:34:20 +0100

    remove threadpoolctl limit - refactor in prep for lazy modules

--- src/resistify/device.py
+++ src/resistify/device.py
@@ -1,16 +1,5 @@
 import os
 import multiprocessing
-import torch
-
-
-def get_device() -> str:
-    if torch.cuda.is_available():
-        return "cuda"
-    elif torch.backends.mps.is_available():
-        return "mps"
-    else:
-        return "cpu"
-
 
 def get_threads() -> int:
     if hasattr(os, "sched_getaffinity"):
