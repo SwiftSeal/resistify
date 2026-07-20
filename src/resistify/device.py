5ab33fa8e7365b67a63f6eb8229dbd1398836fa2
SwiftSeal <moraysmith98@gmail.com>
2026-07-20 13:42:05 +0100

    avoid None in device setting for clarity

--- src/resistify/device.py
+++ src/resistify/device.py
@@ -5,14 +5,14 @@ import torch
 logger = logging.getLogger(__name__)
 
 
-def get_device() -> str:
-    device = ""
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
-    else:
-        device = "cpu"
+def get_device(device: str) -> str:
+    if device == "auto":
+        if torch.cuda.is_available():
+            device = "cuda"
+        elif torch.backends.mps.is_available():
+            device = "mps"
+        else:
+            device = "cpu"
 
-    logging.info(f"Device selected: {device}")
+    logger.info(f"Device selected: {device}")
     return device
